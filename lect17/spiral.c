faf229ac7e8b25761fbcd5323f61264e436ca437
jaya1313 <jayakarwani4@gmail.com>
2025-10-27 00:28:28 +0530

    2d_array

--- lect17/spiral.c
+++ lect17/spiral.c
@@ -10,7 +10,7 @@ int main(){
             }
         }
         }
-    for(int i=2;i>1;i--){
+    for(int i=2;i>1;i--){ 
         for(int j=1;j>=0;j--){
             printf("%d",a[i][j]);  
                  }
