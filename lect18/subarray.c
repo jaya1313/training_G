098cf421fece3e6882aa57ae4a8ca1d0420b8151
jaya1313 <jayakarwani4@gmail.com>
2025-10-27 00:30:36 +0530

    subarray

--- lect18/subarray.c
+++ lect18/subarray.c
@@ -1,20 +1,31 @@
 #include<stdio.h>
 
 // maximum subarray sum of size k
-int main(){
-    int wsum=0;
-    int a[6]={3,4,5,2,1,1};
-    int k=2;
-    for(int i=0;i<k;i++){
-         wsum=wsum+a[i];
-}
-int msum=wsum;
-for(int j=k;j<6;j++){
-    wsum+=a[j]-a[j-k];
-    if(wsum>msum)
-    msum=wsum;
-}
-printf("maximum sum is %d",msum);
-}
+ int main(){
+//     int wsum=0;
+//     int a[6]={3,4,5,2,1,1};
+//     int k=2;
+//     for(int i=0;i<k;i++){
+//          wsum=wsum+a[i];
+// }
+// int msum=wsum;
+// for(int j=k;j<6;j++){
+//     wsum+=a[j]-a[j-k];
+//     if(wsum>msum)
+//     msum=wsum;
+// }
+// printf("maximum sum is %d",msum);
+// }
 
-//
+// first negative number
+int i,j;
+int k=2;
+int a[6]={4,-1,8,-9,-7,5};
+    for(i=0;i<5;i++){
+        for(j=i;j<k+i;j++)
+        if(a[j]<0){
+            printf(" %d",a[j]);
+        }
+
+    }
+}
---
+++ lect25/struct.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+struct student{
+    char name;
+    int rollno;
+    int marks;
+};
+int main(){
+    struct student s1;
+    
+}
