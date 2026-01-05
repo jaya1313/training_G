97bac39a0da0bb16516808c9d73e4f4dbd8143c0
Jaya Karwani <jayakarwani4@gmail.com>
2026-01-05 21:02:27 +0530

    basics

--- lect1/basics.c
+++ lect1/basics.c
@@ -1,10 +1,10 @@
-#include<stdio.h>
+ #include<stdio.h>
 #include<math.h>
 int main(){
 
 
-int s=printf("hello\n");
-printf("%d",s);
+// int s=printf("hello\n");
+// printf("%d",s);
 
 //int a=4;
 //int b=3;
@@ -15,14 +15,14 @@ printf("%d",s);
 //printf("%c\n",ch);
 //printf("%d",ch);
 
-//int a=3;
-//printf("%hu\n",a);
-//printf("%u\n",a);
-//printf("%ld\n",a);
-//printf("%lu\n",a);
-//printf("%zu\n",sizeof(int));
-//printf("%zu\n",sizeof(char));
-//printf("%zu\n",sizeof(float));
+int a=3;
+printf("%hu\n",a);
+printf("%u\n",a);
+printf("%ld\n",a);
+printf("%lu\n",a);
+printf("%zu\n",sizeof(int));
+printf("%zu\n",sizeof(char));
+printf("%zu\n",sizeof(float));
 
 return 0;
 }
