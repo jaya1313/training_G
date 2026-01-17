d33a5ca93059f65fb8527a6763f0c5ac9225145a
Jaya Karwani <jayakarwani4@gmail.com>
2026-01-17 18:52:13 +0530

    2d array

--- lect19/rowReverse.c
+++ lect19/rowReverse.c
@@ -4,26 +4,6 @@ int main(){
 
     //row wise reverse
   
-    // int i,j;
-    // int a[3][3]={{1,2,3},{4,5,6},{7,8,9}};
-    // for(i=0;i<3;i++){
-    //     for(j=0;j<3;j++){
-    //         printf(" %d",a[i][j]);
-    //        }
-    //        printf("\n");
-    // }
-    //  printf("\n");
-
-    // for(i=0;i<3;i++){
-    //     for(j=2;j>=0;j--){
-    //         printf(" %d",a[i][j]);
-    //        }
-    //        printf("\n");
-    // }
-
-
-    //column reverse
-
     int i,j;
     int a[3][3]={{1,2,3},{4,5,6},{7,8,9}};
     for(i=0;i<3;i++){
@@ -34,12 +14,33 @@ int main(){
     }
      printf("\n");
 
-    for(i=2;i>=0;i--){
-        for(j=0;j<3;j++){
+    for(i=0;i<3;i++){
+        for(j=2;j>=0;j--){
             printf(" %d",a[i][j]);
            }
            printf("\n");
     }
+}
+
+
+    //column reverse
+
+//     int i,j;
+//     int a[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+//     for(i=0;i<3;i++){
+//         for(j=0;j<3;j++){
+//             printf(" %d",a[i][j]);
+//            }
+//            printf("\n");
+//     }
+//      printf("\n");
+
+//     for(i=2;i>=0;i--){
+//         for(j=0;j<3;j++){
+//             printf(" %d",a[i][j]);
+//            }
+//            printf("\n");
+//     }
 
    
-}
+// }
